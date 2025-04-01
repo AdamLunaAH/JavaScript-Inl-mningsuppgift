68e2ef159b3de920f5381c63e62e63186a81aa76
AdamLunaAH <adam.a-h@outlook.com>
2025-04-01 11:42:04 +0200

    s

---
+++ script/site-scripts/groupinfo.js
@@ -0,0 +1,90 @@
+"use strict";
+
+import musicService from "../database-script/music-group-services.js";
+
+const _service = new musicService(
+    `https://seido-webservice-307d89e1f16a.azurewebsites.net/api`
+);
+
+// const linkString = window.location.href;
+// console.log(linkString);
+
+const urlParams = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => {
+        return searchParams.get(prop);
+    },
+});
+
+console.log(urlParams.id);
+
+let groupData = [];
+
+let artistData = [];
+let albumData = [];
+
+async function getMusicGroupData() {
+    // data = await _service.readMusicGroupsAsync(0, true);
+    groupData = await _service.readMusicGroupDtoAsync(urlParams.id);
+    console.log(groupData);
+}
+
+async function getMusicGroupArtists(artistId) {
+    artistData = await _service.readArtistDtoAsync(artistId);
+    // console.log(artistData);
+    // return artistData;
+}
+
+await getMusicGroupData();
+
+(async () => {
+    window.onload = showGroups();
+
+    //Helpers
+    async function showGroups() {
+        //Clear first
+
+        // await getMusicGroupData();
+
+        const groupInfo = document.querySelector("#group-info");
+        groupInfo.classList.add("col-md-12", "themed-grid-col");
+        // showGroups
+        //creat a row for every quote and append it to _list
+        // for (const item of groupData) {
+        // const div = createRow();
+        const div = document.createElement("div");
+        // div.classList.add("col-md-12", "themed-grid-col");
+
+        const h1 = document.createElement("h1");
+        h1.classList.add("display-5", "fw-bold", "lh-1", "mb-3");
+        h1.innerText = groupData.name;
+        div.appendChild(h1);
+
+        const p = document.createElement("p");
+        p.classList.add("lead", "fw-bold", "mb-3");
+        p.innerText = groupData.establishedYear;
+        div.appendChild(p);
+
+        // p.classList.add("lead", "fw-bold", "mb-3");
+        // div.innerText = item.name + " " + item.establishedYear + " " + item.musicGroupId ;
+
+        // div.appendChild(link);
+
+        groupInfo.appendChild(div);
+        await groupArtists();
+
+        // }
+
+        async function groupArtists() {
+            const artistDiv = document.createElement("div");
+            for (const artist of groupData.artistsId) {
+                await getMusicGroupArtists(artist);
+                const artistsP = document.createElement("p");
+                artistsP.classList.add("lead", "fw-bold", "mb-3");
+                artistsP.innerText =
+                    artistData.firstName + " " + artistData.lastName;
+                artistDiv.appendChild(artistsP);
+            }
+            groupInfo.appendChild(artistDiv);
+        }
+    }
+})();
--- script/site-scripts/musicgroups.js
+++ script/site-scripts/musicgroups.js
@@ -25,58 +25,36 @@ async function getMusicGroupsData() {
 
 
 
+
+
 await getMusicGroupsData();
 
 (async () => {
 
     window.onload = showGroups();
 
-    // let data = await _service.readMusicGroupsAsync(currentPageNr, true);
-
-    // const pageSize = 10;
-
-    // const maxPageNr = Math.ceil(data.length / pageSize);
-
     console.log(data.pageItems?.length || 0);
 
-    // console.log(maxPageNr);
 
-    // let currentPageNr = 0;
 
     const _list = document.querySelector("#list-of-items");
-    const clearBtn = document.querySelector("#clearBtn");
-    const allQ = document.querySelector("#allQBtn");
-    // // const loveQ = document.querySelector("#loveQBtn");
+
 
     const btnPrev = document.querySelector("#prevBtn");
     const btnNext = document.querySelector("#nextBtn");
 
-    clearBtn.addEventListener("click", clickHandlerClear);
-    allQ.addEventListener("click", clickHandlerAllQ);
-    // loveQ.addEventListener("click", clickHandlerLoveQ);
+
 
     btnPrev.addEventListener("click", clickHandlerPrev);
     btnNext.addEventListener("click", clickHandlerNext);
 
     function clickHandlerAllQ(e) {
-        // let _groups = data.name.slice(
-        //     currentPageNr * pageSize,
-        //     currentPageNr * pageSize + pageSize
-        // );
-
-        // fillList(_groups);
 
         clearList();
         showGroups();
     }
 
-    // function clickHandlerLoveQ(e) {
-    //     //filter out all love quotes from _seeder.allQuote
-    //     let _groups = data.filter((item) =>
-    //         item.group.toLowerCase().includes("love")
-    //     );
-    //     fillList(_groups);
-    // }
+
 
     function clickHandlerClear(e) {
         clearList();
@@ -84,9 +62,7 @@ await getMusicGroupsData();
 
     // //Paging
     function clickHandlerNext(e) {
-        // if (currentPageNr < maxPageNr - 1) {
             currentPageNr++;
-        // }
         clickHandlerAllQ();
     }
 
@@ -97,19 +73,52 @@ await getMusicGroupsData();
         clickHandlerAllQ();
     }
 
-    // //Helpers
-    // function fillList(_groups) {
-    //     //Clear first
-    //     clearList();
+    //Helpers
+    async function showGroups() {
 
-    //     showGroups
-    // //     //creat a row for every quote and append it to _list
-    // //     for (const item of groups) {
-    // //         const div = createRow();
-    // //         div.innerText = item.name;
-    // //         _list.appendChild(div);
-    // //     }
+
+        await getMusicGroupsData();
+
+
+        // showGroups
+        //creat a row for every quote and append it to _list
+        for (const item of data.pageItems) {
+            // const div = createRow();
+            const div = document.createElement("div");
+            div.classList.add("col-md-12", "themed-grid-col");
+
+
+
+
+            div.innerText = item.name + " " + item.establishedYear;
+            // div.innerText = item.establishedYear;
+            const link = document.createElement("a");
+            link.href = `groupinfo.html?id=${item.musicGroupId}`;
+            link.innerText = "Go to group info";
+            link.classList.add("btn", "btn-primary", "btn-sm", "btn-block");
+            link.setAttribute("role", "button");
+            link.setAttribute("aria-pressed", "true");
+
+
+            div.appendChild(link);
+
+            _list.appendChild(div);
+        }
+    }
+
+
+    // async function showGroups() {
+    //     await getMusicGroupsData();
+
+    //     for (const item of data.pageItems) {
+    //         const li = document.createElement("li");
+    //         li.innerText = `${item.name}`;
+    //         _list.appendChild(li);
+    //     }
+
+    //     console.log(currentPageNr);
     // }
+
 
     function clearList() {
         while (_list.firstChild) {
@@ -117,29 +126,20 @@ await getMusicGroupsData();
         }
     }
 
-    // function createRow() {
-    //     const div = document.createElement("div");
-    //     div.classList.add("col-md-12", "themed-grid-col");
-    //     return div;
-    // }
-
-
-
-
-    async function showGroups() {
-
-        await getMusicGroupsData();
-
-        for (const item of data.pageItems) {
-            const li = document.createElement("li");
-            li.innerText = `${item.name}`;
-            _list.appendChild(li);
-        }
-
-        console.log(currentPageNr);
+    function createRow() {
+        const div = document.createElement("div");
+        div.classList.add("col-md-12", "themed-grid-col");
+        return div;
     }
 
-    // showGroups();
+
+
+
+
+
+
+
+
 
 
 })();
